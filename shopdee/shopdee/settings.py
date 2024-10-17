aa8a7ec14eaebfdf2706d94dcf72c83b6cc9f222
Pisol Uattankanjana <pisol.kanomjeen.uatt@gmail.com>
2024-10-17 13:24:55 +0700

    Update settings.py

--- shopdee/shopdee/settings.py
+++ shopdee/shopdee/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': 'shopdee_db',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': 'localhost',  # ชื่อบริการที่ระบุใน docker-compose.yml
+        'HOST': '34.87.47.244',  # ชื่อบริการที่ระบุใน docker-compose.yml
         'PORT': '5432',
     }
 }
