4ef64e4b932803106d98444073305a28227c6a9b
Pisol Uattankanjana <pisol.kanomjeen.uatt@gmail.com>
2024-10-17 13:45:22 +0700

    Update settings.py

--- shopdee/shopdee/settings.py
+++ shopdee/shopdee/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
+
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = (
